c81cb898b76951d13d5b029ac1fb316eefc378bb
John Ellis <deckerego@gmail.com>
2013-06-07 17:09:23 -0400

    Updated PetaPoco assembly info to version 0.0.0.0

--- PetaPoco.Tests/Properties/AssemblyInfo.cs
+++ PetaPoco.Tests/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("211.00.00.00")]
-[assembly: AssemblyVersion("211.00.00.00")]
-[assembly: AssemblyFileVersion("211.00.00.00")]
+// [assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
